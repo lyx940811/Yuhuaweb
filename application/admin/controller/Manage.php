84752479c2c8480312d5139f562f4fabdf977a45
limin <510141290@qq.com>
2017-12-06 10:27:05 +0800

    后台

--- application/admin/controller/Manage.php
+++ application/admin/controller/Manage.php
@@ -1,7 +1,16 @@
 <?php
+namespace app\admin\controller;
 /**
- * Created by PhpStorm.
- * User: jason
+ * Created by phpstorm.
+ * User: m's
  * Date: 2017/12/5
  * Time: 17:16
+ *
  */
+
+class Manage extends Base{
+
+    public function index(){
+        return view('index');
+    }
+}
