5feb40ef3d443e68558739ae9487ee8088c3b573
limin <510141290@qq.com>
2017-12-26 15:55:19 +0800

    专业报名数据查询

---
+++ application/manage/controller/Student_enroll.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: M'S
+ * Date: 2017/12/26
+ * Time: 15:52
+ */
+namespace app\manage\controller;
+
+class Student_enroll extends Base{
+    public function index(){
+
+
+        
+        return $this->fetch();
+    }
+}
