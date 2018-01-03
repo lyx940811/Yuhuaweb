adccadd2f8c5b9bae2ac2098c5ab7dd6f11d205d
limin <510141290@qq.com>
2018-01-03 15:17:29 +0800

    班级管理

--- application/manage/controller/Classroom.php
+++ application/manage/controller/Classroom.php
@@ -20,9 +20,9 @@ class Classroom extends Base{
 
         $where = [];
 
-        if(!empty($info['flag'])){
+        if(!empty($info['title'])){
 
-            $where['Flag'] = ['eq',$info['flag']];
+            $where['a.title'] = ['like',"%{$info['title']}%"];
         }
 
         $list = Db::name('classroom a')
@@ -91,10 +91,10 @@ class Classroom extends Base{
         if(isset($_GET['do'])=='get'){
             $id = $_GET['rid']+0;
 
-            $have = Db::name('category')->field('id,name,code,point,studyTimes,description,Flag')->where("id='$id'")->find();
+            $have = Db::name('classroom')->field('id,title,categoryId,teacherIds,about')->where("id='$id'")->find();
 
             if(!$have){//如果这个code有
-                return ['error'=>'没有此专业','code'=>'300'];
+                return ['error'=>'没有此班级','code'=>'300'];
             }else{
                 return ['info'=>$have,'code'=>'000'];
             }
@@ -104,19 +104,12 @@ class Classroom extends Base{
         $info = input('post.');
 
         $msg  =   [
-            'rid.require' => '专业名称rid不能为空',
-            'name.require' => '专业名称不能为空',
-            'name.length' => '专业名称长度太短',
-            'code.require' => '代码不能为空',
-            'code.number' => '代码必须为数字',
-            'point.number' => '学分必须为数字',
+            'title.require' => '名称不能为空',
+            'title.length' => '名称长度太短',
         ];
 
         $validate = new Validate([
-            'rid'    => 'require',
-            'name'   => 'require|length:2,20',
-            'code'   => 'require|number',
-            'point'  => 'number'
+            'title'  => 'require|length:2,20',
         ],$msg);
 
         $validate->check($info);
@@ -127,31 +120,26 @@ class Classroom extends Base{
             return ['error'=>$error,'code'=>'200'];
         }
 
-        $role_table = Db::name('category');
+        $role_table = Db::name('classroom');
 
         $id = $info['rid']+0;
-        $have = $role_table->field('id')->where("id='$id'")->find();
 
-        if(!$have){//如果没这个code
-            return ['error'=>'没有此专业','code'=>'300'];
-        }
-
-        $have = $role_table->field('id,code')->where("id <> $id AND code={$info['code']}")->find();
-
-        if($have){
-            return ['error'=>'已经有此代码','code'=>'300'];
-        }
+//        $have = $role_table->field('id')->where("id='$id'")->find();
+//
+//        if(!$have){//如果没这个code
+//            return ['error'=>'没有此班级','code'=>'300'];
+//        }
 
         $data = [
-            'name'=>$info['name'],
-            'code'=>$info['code'],
-            'point'=>$info['point'],
-            'studyTimes'=>$info['studyTimes'],
-            'description'=>$info['description'],
-//            'createtime'=>date('Y-m-d H:i:s',time())
+            'title' => $info['title'],
+            'categoryId' => $info['categoryId'],
+            'teacherIds'=> $info['teacherIds'],
+            'about'=>$info['about'],
+//            'createdTime'=>date('Y-m-d H:i:s',time()),
+            'status'=>1,
         ];
 
-        $ok = $role_table->field('name,code,point,studyTimes,description,createtime')->where('id',$id)->update($data);
+        $ok = $role_table->field('title,categoryId,teacherIds,about,status')->where('id',$id)->update($data);
 
         if($ok){
             return ['info'=>'修改成功','code'=>'000'];
@@ -160,17 +148,16 @@ class Classroom extends Base{
         }
     }
 
-    public function enable(){
+    public function delete(){
 
         $id = $_GET['rid']+0;
 
-        $data = ['Flag'=>0,'createtime'=>date('Y-m-d H:i:s',time())];
-        $ok = Db::name('category')->field('Flag,createtime')->where("id='$id'")->update($data);
+        $ok = Db::name('classroom')->where("id='$id'")->delete();
 
         if($ok){
-            return ['info'=>'禁用成功','code'=>'000'];
+            return ['info'=>'删除成功','code'=>'000'];
         }else{
-            return ['error'=>'禁用失败','code'=>'200'];
+            return ['error'=>'删除失败','code'=>'200'];
         }
     }
 
