2b52ff9b51d9fdbc875b1b2e10cdc93926e4dcde
limin <510141290@qq.com>
2018-01-05 09:57:42 +0800

    基础控制器

--- application/manage/controller/Base.php
+++ application/manage/controller/Base.php
@@ -26,7 +26,7 @@ class Base extends Controller{
         $role = check($this->uid);
 
         if(!$role){
-            $this->error('您没权限操作!',url('Manage/login/index'),['code'=>200]);
+            $this->error('您没权限操作!',url('Manage/login/index'),['code'=>120]);
 //            return ['error'=>'您没权限查看','url'=>url('Manage/login/index')];
         }
 
