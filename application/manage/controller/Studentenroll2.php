8784ee6c26338593fa57ca1e72781dbb369a5e55
limin <510141290@qq.com>
2018-01-08 14:25:59 +0800

    学生报名管理查看授理

--- application/manage/controller/Studentenroll2.php
+++ application/manage/controller/Studentenroll2.php
@@ -31,14 +31,14 @@ class StudentEnroll2 extends Base{
             ->join('category b','a.categoryID=b.code','LEFT')
             ->join('admission c','a.admissionID=c.id','LEFT')
             ->where($where)
-            ->field('a.id,a.realname,a.sex,a.telephone as phone,a.admissionID,b.name,a.createTime,a.status,c.title')->paginate(20,false,['query'=>request()->get()]);
+            ->field('a.id,a.realname,a.sex,a.telephone as phone,b.name,a.createTime,a.age,a.promotMan,a.admissionID,a.school,a.address,c.title')->paginate(20,false,['query'=>request()->get()]);
 
 //        echo Db::table('student_enroll')->getLastSql();exit;
 
         $category = Db::table('category')->field('code,name')->where('Flag','eq',1)->select();
 
         $admission = Db::table('admission')->field('id,title')->select();
-        $this->assign('typename','专业报名数据查询');
+        $this->assign('typename','报名管理');
 
         $this->assign('list',$list);
         $this->assign('admission',$admission);
@@ -46,5 +46,24 @@ class StudentEnroll2 extends Base{
         $this->assign('page',$list->render());
 
         return $this->fetch();
+    }
+
+    public function accept(){
+        $id = $_GET['rid']+0;
+
+        if($_GET['type']==2){
+            $s['status'] = 2;
+        }else{
+            $s['status'] = 1;
+        }
+
+        $ok = Db::name('student_enroll')->field('status')->where('id',$id)->update($s);
+
+        if($ok){
+            return ['info'=>'授理成功','code'=>'000'];
+        }else{
+            return ['error'=>'授理失败','code'=>'200'];
+        }
+
     }
 }
