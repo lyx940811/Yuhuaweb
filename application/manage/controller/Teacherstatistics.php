ebf39660bbe20e339d7e1ae1858bf5c5a3548dfa
tiantian2018 <510141290@qq.com>
2018-02-06 16:58:25 +0800

    教师统计添加18020616：58

--- application/manage/controller/Teacherstatistics.php
+++ application/manage/controller/Teacherstatistics.php
@@ -15,19 +15,39 @@ class Teacherstatistics extends  Base{
         $list = Db::table('teacher_info')
             ->paginate(20);
 
-
         $newarr = [];
+        $totalmedia = 0;
+        $totalcheck = 0;
+        $totalanswer = 0;
+        $totalask = 0;
         foreach ($list->items() as $k=>$v){
-//            echo $v['id']."<br/>";
             $newarr[$k] = $v;
-            $newarr[$k]['teachnum'] = Db::table('course')->where('teacherIds',$v['id'])->value('count(teacherIds) as num');
-            $newarr[$k][''];
+            $newarr[$k]['teachnum'] = Db::table('course')->where('teacherIds',$v['id'])->value('count(id) as num');
+            $newarr[$k]['medianum'] = Db::table('course_task a')
+                ->join('course b','a.courseId=b.id','LEFT')->where('b.teacherIds',$v['id'])
+                ->value('count(a.id) as num');
+            $totalmedia +=$newarr[$k]['medianum'];
+            $newarr[$k]['checknum'] = Db::table('testpaper_result')->where('checkTeacherId',$v['id'])->value('count(id) as num');
+            $totalcheck +=$newarr[$k]['checknum'];
+            $newarr[$k]['answernum'] = Db::table('ask_answer')->where('answerUserId',$v['id'])->value('count(id) as num');
+            $totalanswer += $newarr[$k]['answernum'];
+            $newarr[$k]['asknum'] = Db::table('asklist')->where('userID',$v['id'])->value('count(id) as num');
+            $totalask +=$newarr[$k]['asknum'];
         }
 
-        print_r($newarr);exit;
 
-        $this->assign('list',$list);
+        $totalteacher = $list->total();
+
+
+        $this->assign('list',$newarr);
         $this->assign('page',$list->render());
+        $this->assign('typename','教师统计');
+        $this->assign('totalteacher',$totalteacher);
+        $this->assign('totalmedia',$totalmedia);
+        $this->assign('totalcheck',$totalcheck);
+        $this->assign('totalanswer',$totalanswer);
+        $this->assign('totalask',$totalask);
+        $this->assign('uid',session('admin_uid'));
         return $this->fetch();
     }
 }
